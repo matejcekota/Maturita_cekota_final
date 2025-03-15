926ebc701c615577dfcc30cc9417e897fda8386f
matěj cekota <d21770@oaopava.cz>
2025-03-15 17:23:51 +0100

    qwww

--- registrace.php
+++ registrace.php
@@ -1,19 +1,19 @@
 <?php
-// Připojení k databázi
-$servername = "localhost";
-$username = "root"; // Zadejte vaše uživatelské jméno
-$password = "databazematurita22"; // Zadejte vaše heslo
+
+/*$servername = "localhost";
+$username = "root";
+$password = "databazematurita22"; 
 $dbname = "registrace_motivace";
 
-// Vytvoření připojení
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Zkontrolujte připojení
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Zpracování formuláře
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $jmeno = $conn->real_escape_string($_POST['jmeno']);
     $prijmeni = $conn->real_escape_string($_POST['prijmeni']);
@@ -21,17 +21,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $heslo = $conn->real_escape_string($_POST['heslo']);
     $pohlavi = $conn->real_escape_string($_POST['pohlavi']);
 
-    // Hashování hesla
+
     $hashed_password = password_hash($heslo, PASSWORD_BCRYPT);
 
-    // Kontrola, zda už email neexistuje
+
     $sql_check = "SELECT * FROM registrace WHERE email = '$email'";
     $result = $conn->query($sql_check);
 
     if ($result->num_rows > 0) {
         echo "Email již existuje!";
     } else {
-        // Vložení nového uživatele do databáze
+
         $sql = "INSERT INTO registrace (jmeno, prijmeni, pohlavi, email, heslo) 
                 VALUES ('$jmeno', '$prijmeni', '$pohlavi', '$email', '$hashed_password')";
 
